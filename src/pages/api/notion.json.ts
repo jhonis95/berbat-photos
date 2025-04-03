15638db00c90678f895bf76fe5378a6830ca4d80
Jonatan <j.jhonis95@gmail.com>
2025-04-03 15:47:47 -0300

    fixing home page responsivity

--- src/pages/api/notion.json.ts
+++ src/pages/api/notion.json.ts
@@ -8,4 +8,17 @@ const notion = new Client({
 export async function GET(){
     const response = await notion.databases.retrieve({ database_id: NOTION_DB_ID });
     console.log(response);
+    
+    if (!response.ok) {
+      return new Response(JSON.stringify({ error: "Failed to fetch images" }), {
+        status: response.status,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    const data = await response.json();
+
+    return new Response(JSON.stringify(data.resources), {
+      headers: { "Content-Type": "application/json" },
+    });
 }
