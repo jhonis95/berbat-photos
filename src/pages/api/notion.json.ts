8aff373d561149ed6deb5fd6863b6a465d184dfb
Jonatan <j.jhonis95@gmail.com>
2025-04-04 11:29:18 -0300

    make page review responsive

--- src/pages/api/notion.json.ts
+++ src/pages/api/notion.json.ts
@@ -1,4 +1,5 @@
-const { Client } = require("@notionhq/client")
+import { Client } from "@notionhq/client";
+
 const NOTION_TOKEN = import.meta.env.NOTION_TOKEN;
 const NOTION_DB_ID = import.meta.env.NOTION_DB_ID;
 const notion = new Client({
@@ -9,16 +10,14 @@ export async function GET(){
     const response = await notion.databases.retrieve({ database_id: NOTION_DB_ID });
     console.log(response);
     
-    if (!response.ok) {
+    if (!response) {
       return new Response(JSON.stringify({ error: "Failed to fetch images" }), {
-        status: response.status,
+        status: response,
         headers: { "Content-Type": "application/json" },
       });
     }
 
-    const data = await response.json();
-
-    return new Response(JSON.stringify(data.resources), {
+    return new Response(JSON.stringify(response), {
       headers: { "Content-Type": "application/json" },
     });
 }
