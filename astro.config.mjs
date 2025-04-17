ce557c88f8839061d3f16dc771efece6e948e139
Jonatan <j.jhonis95@gmail.com>
2025-04-17 15:28:34 -0300

    adding emailjs to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import alpinejs from "@astrojs/alpinejs";
+import netlify from '@astrojs/netlify';
 // https://astro.build/config
 export default defineConfig({
   vite: {
--- env.d.ts
+++ env.d.ts
@@ -2,6 +2,10 @@ interface ImportMetaEnv {
     readonly PUBLIC_CLOUDINARY_CLOUD_NAME: string;
     readonly PUBLIC_CLOUDINARY_API_KEY: string;
     readonly CLOUDINARY_API_SECRET: string;
+
+    readonly PUBLIC_EMAILJS_SERVICE_ID: string;
+    readonly PUBLIC_EMAILJS_TEMPLATE_ID: string;
+    readonly PUBLIC_EMAILJS_USER_ID: string;
   }
   
   interface ImportMeta {
