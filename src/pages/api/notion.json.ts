73e669ea75e22fcc3af7c5c8748a94365122fae5
Jonatan <j.jhonis95@gmail.com>
2025-04-24 10:13:00 -0300

    optimize all images

--- src/pages/api/notion.json.ts
+++ src/pages/api/notion.json.ts
@@ -9,7 +9,6 @@ const notion = new Client({
 
 export async function GET(){
     const response = await notion.databases.retrieve({ database_id: NOTION_DB_ID });
-    console.log(response);
     
     if (!response) {
       return new Response(JSON.stringify({ error: "Failed to fetch images" }), {
