de1820c06d9d9f1ce60454e284cb390d8d9024a7
Jonatan <j.jhonis95@gmail.com>
2025-07-27 04:53:40 -0300

    new i18 index to google search

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,7 @@ export default defineConfig({
   // }),
   integrations: [alpinejs(),sitemap({
     i18n: {
-        defaultLocale: 'fr', // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+        defaultLocale: 'en', // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
         locales: {
           en: 'en-US', // The `defaultLocale` value must present in `locales` keys
           fr: 'fr-FR',
